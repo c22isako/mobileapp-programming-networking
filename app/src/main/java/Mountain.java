a4e8af6b8c85100107a3132627a5626fe8aaff06
c22isako <c22isako@student.his.se>
2024-04-19 15:00:19 +0200

    Created a new class, Mountain.java + some parameters

---
+++ app/src/main/java/Mountain.java
@@ -0,0 +1,24 @@
+
+public class Mountain {
+
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(String name,String location,int height) {
+
+        name = "no name";
+        location = "no location";
+        height = -1;
+
+    }
+    public Mountain(String name) {
+
+        name = "no name";
+        location = null;
+        height = 0;
+
+    }
+
+
+}
